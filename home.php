4ba38fc706e56570b824625083001cbe9d0df34c
Kaia Mia Kalda <kaiamia@users.noreply.github.com>
2022-10-10 20:13:58 +0300

    Viies kodutöö

---
+++ home.php
@@ -0,0 +1,17 @@
+<?php
+$author_name = "Kaia Mia Kalda"
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title><?php echo $author_name; ?>, veebiprogrammeerimine</title>
+</head>
+<body>
+	<img src="pildid/vp_banner_gs.png" alt="Banner">
+	<h1><?php echo $author_name; ?>, veebiprogrammeerimine</h1>
+	
+	<p>See leht on loodud õppetöö raames ja ei sisalda tõsist infot!</p>
+	<p>Õppetöö toimus <a href="https://www.tlu.ee">Tallinna Ülikoolis</a>, Digitehnoloogiate instituudis.</p>
+</body>
+</html>
